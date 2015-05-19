cb52b664e89a059e81d6cfe7566e00a5025574b6
revanth47 <7revanth47@gmail.com>
2015-05-19 10:16:50 +0530

    task 1 111114075

---
+++ js_task1.js
@@ -0,0 +1,19 @@
+var ch_colour = function(n)
+{
+	var colour=["red","blue","green","yellow","orange","black","aqua","blue","purple","pink"];
+	var x;
+    if(n===1)
+	{
+
+		x=Math.floor(10*Math.random());
+       return colour[x];
+	}
+	else if(n===2)
+	{
+
+		x=Math.floor(10*Math.random());
+       return colour[x];
+	}
+
+
+}
